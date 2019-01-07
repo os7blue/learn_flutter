020b1a3070c92a5fd8650f400fd6d1e90acc03d0
os7blue <os7blue@163.com>
2019-01-07 12:14:24 +0800

    23333

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class RandomWordsState extends State<RandomWords> {
             (pair) {
               return new ListTile(
                 title: new Text(
-                  pair.asPascalCase+"轩文杰智障",
+                  pair.asPascalCase,
                   style: _biggerFont,
                 ),
               );
